ac81a49aa2eaeacde880d7d93d0ce7f77fd1ebe9
evandro <evaandro@msn.com>
2022-01-13 12:29:56 -0300

    Ajustes identacao | JS | CSS.

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ const receivePassword = document.getElementById('password');
 const btn = document.getElementById('btn');
 const btnSend = document.getElementById('submit-btn');
 const agreeCheck = document.getElementById('agreement');
-const keyBoardCheck = document.getElementById('textarea')
+const keyBoardCheck = document.getElementById('textarea');
+const counter = document.getElementById('counter');
 
-btn.addEventListener('click', function () {
-  if (receiveEmail.value == "tryber@teste.com" && receivePassword.value == "123456")  {
-    alert("Olá, Tryber!");        
+btn.addEventListener('click', () => {
+  if (receiveEmail.value === 'tryber@teste.com' && receivePassword.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-      alert("Email ou senha inválidos."); 
-  }    
-} );
-/*REQUISITO 18*/
-agreeCheck.addEventListener('click', function () {
-  console.log(agreeCheck.value)
-  if (agreeCheck.value != 'on') {
-    btnSend.removeAttribute('disabled')
-  } else 
-      btnSend.toggleAttribute('disabled')
+    alert('Email ou senha inválidos.');
+  }
 });
-/*REQUISITO 20*/
-keyBoardCheck.addEventListener('input', function () {
+/* REQUISITO 18 */
+agreeCheck.addEventListener('click', () => {
+  console.log(agreeCheck.value);
+  if (agreeCheck.value !== 'on') {
+    btnSend.removeAttribute('disabled');
+  } else { btnSend.toggleAttribute('disabled'); }
+});
+/* REQUISITO 20 */
+keyBoardCheck.addEventListener('input', () => {
   const maxChar = 500;
   const charCounter = keyBoardCheck.value.length;
   const charDecrement = maxChar - charCounter;
   counter.innerHTML = charDecrement;
-});  
+});
