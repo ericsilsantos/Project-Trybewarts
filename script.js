ff9088d3d802c9a739b2b67f2f50fa304aeee177
Eric Santos <eric.silvasantos44@gmail.com>
2022-01-17 17:27:50 -0300

    Adicionado o requisito 21

--- script.js
+++ script.js
@@ -18,7 +18,9 @@ agreeCheck.addEventListener('click', () => {
   console.log(agreeCheck.value);
   if (agreeCheck.value !== 'on') {
     btnSend.removeAttribute('disabled');
-  } else { btnSend.toggleAttribute('disabled'); }
+  } else { 
+    btnSend.toggleAttribute('disabled'); 
+  }
 });
 /* REQUISITO 20 */
 keyBoardCheck.addEventListener('input', () => {
@@ -27,3 +29,34 @@ keyBoardCheck.addEventListener('input', () => {
   const charDecrement = maxChar - charCounter;
   counter.innerHTML = charDecrement;
 });
+// REQUISITO 21
+function selectedMaterials(){
+  let materials=""
+  const selected=document.querySelectorAll('.subject:checked')
+  for(let index=0;index<selected.length; index++){
+    materials+=`${selected[index].value}, `
+  }
+  return materials
+}
+function createArray(){
+  let array=[]
+  array[0]="Nome: "+document.getElementById("input-name").value+" "+document.getElementById("input-lastname").value
+  array[1]="Email: "+document.getElementById("input-email").value
+  array[2]="Casa: "+document.getElementById("house").value
+  array[3]="Família: "+document.querySelector('input[name="family"]:checked').value
+  array[4]="Matérias: "+selectedMaterials()
+  array[5]="Avaliação: "+document.querySelector('input[name="rate"]:checked').value
+  array[6]="Observações: "+document.getElementById("textarea").value
+  return array
+}
+btnSend.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(document.querySelectorAll('.subject:checked'))
+  let arrayForm=createArray()
+  for(let index=0; index<arrayForm.length; index++){
+    let form=document.getElementById("evaluation-form")
+    let p=document.createElement("p")
+    p.innerText=arrayForm[index]
+    form.appendChild(p)
+  }
+})
